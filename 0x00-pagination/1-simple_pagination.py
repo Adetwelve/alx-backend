5d25bbb508f9e79acbc856cd9bdcd2dc6bdc0ed6
Adetwelve <niyero10@gmail.com>
2023-07-24 03:02:50 +0000

    task 1:Simple pagination fix

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
-""" A Simple helper function """
+"""Simple pagination sample.
+"""
 import csv
-import math
 from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ A function that return start idex and
-        end index of a particular pageination
+    """Retrieves the index range from a given page and page size.
     """
-    if page <= 1:
-        offset = 0
-    else:
-        offset = (page - 1) * page_size
-
-    limit = page * page_size
-    return offset, limit
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
@@ -24,9 +19,13 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -36,16 +35,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ A method to get specific page of dataset """
-        assert isinstance(page, int) and page > 0, "should_err"
-        assert isinstance(page_size, int) and page_size > 0, "should_err"
-
-        offset, limit = index_range(page, page_size)
-        dataset = self.dataset()
-
-        # Check if the offset is out of dataset range
-        if offset >= len(dataset):
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
             return []
-
-        # Slice the dataset to get the specified page
-        return dataset[offset:limit]
+        return data[start:end]
