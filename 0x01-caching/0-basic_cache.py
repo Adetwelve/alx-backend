973511624bc45fad413be61de49644a56b5544de
Adetwelve <niyero10@gmail.com>
2023-07-27 01:02:58 +0000

    task 0, README, Parentclass

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" BasicCache module """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class
+      - inherete BaseCaching
+    """
+
+    def put(self, key, item):
+        """ assign value to a key """
+        if key != None or item != None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ get value of the key """
+        return self.cache_data.get(key, None)
+
+
+
+
+            
+
