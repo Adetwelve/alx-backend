fb888170c949f28be44533049b4bd88dcf8b95f5
Adetwelve <niyero10@gmail.com>
2023-07-27 03:54:20 +0000

    task 2 lifo

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/bin/usr/env python3
+""" LIFOCache module """
+BaseCaching = __import__('base_caching').BaseCaching
+from collections import OrderedDict
+
+class LIFOCache(BaseCaching):
+    """ LIFO Cache class Inheriting BaseCaching """
+
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Update the cache """
+        if key is None and item is None:
+            return
+        
+        if key not in self.cache_data:
+            if len(self.cache_data)+ 1 > BaseCaching.MAX_ITEMS:
+                last_key, last_item = self.cache_data.popitem(True)
+                print(f'DISCARD: {last_key}')
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """ Get key from the cache """
+        return self.cache_data.get(key, None)
