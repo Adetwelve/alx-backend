2b067a550709b0b7725ea45713858b878d99fdc8
Adetwelve <niyero10@gmail.com>
2023-08-07 03:02:16 +0100

    task 6: Use user locale

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""A Basic Flask app with internationalization support.
+"""
+from flask_babel import Babel
+from flask import Flask, render_template, request, g
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """check the 'lang' parameter in the url
+    """
+    locale = request.args.get('locale')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    """ check for user preferred local in mock database """
+    if g.user and g.user.get('locale') in app.config["LANGUAGES"]:
+        return g.user['locale']
+    """ Locale from request header """
+    header_locale = request.headers.get('locale')
+    if header_locale:
+        return header_locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user(user_id):
+    """ get user id """
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    """ set user id as global """
+    user_id = request.args.get("login_as")
+    if user_id:
+        user_id = int(user_id)
+        g.user = get_user(user_id)
+    else:
+        g.user = None
+
+
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('6-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
